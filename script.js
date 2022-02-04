a646e0f26c0adb2cd7d9817aa34784a5d5cdaec9
Jonah Milner <jonahmilner86@gmail.com>
2022-02-04 10:29:21 -0700

    added some if statements for character choice, somehow it is working

--- script.js
+++ script.js
@@ -5,6 +5,37 @@ let lowersArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','
 let specialChars = ['!','@','#','$','%','^','&','*','<','>','?'];
 
 
+const randomItems = (Array) => {
+  return Array[Math.floor(Math.random() * Array.length)]
+};   const charLoop = () => {
+    let charChoice = {
+      capsLetters: window.confirm("Confirm to add capital letters to your password"),
+      lowersLetters: window.confirm("Confirm to add Lower Case letters to your password"),
+      includeNumbers: window.confirm("Confirm to add numbers to your password"),
+      specials: window.confirm("Confirm to add special characters to your password")
+    }
+   
+    if (charChoice.capsLetters) {
+      let tempVar =  randomItems(capsArray)
+        console.log(tempVar)
+    }
+    if (charChoice.lowersLetters) {
+      let tempVar = randomItems(lowersArray)
+      console.log(tempVar)
+    }
+    if (charChoice.includeNumbers) {
+      let tempVar = randomItems(numArray) 
+      console.log(tempVar)
+    }
+    if(charChoice.specials) {
+      let tempVar = randomItems(specialChars)
+      console.log(tempVar)
+    }
+    else {
+      window.alert("You must choose at least one Character option!")
+      charLoop();
+    }
+}
 const generatePassword = () => {
     let passwordLength = window.prompt("What is your desired password length? (8-128 characters). Please choose a number.")
     
@@ -18,23 +49,7 @@ const generatePassword = () => {
       generatePassword();
     };
 
-    let charChoice = {
-      capsLetters: window.confirm("Confirm to add capital letters to your password"),
-      lowersLetters: window.confirm("Confirm to add Lower Case letters to your password"),
-      includeNumbers: window.confirm("Confirm to add numbers to your password"),
-      specials: window.confirm("Confirm to add special characters to your password")
-    }
-
-    if (charChoice.capsLetters) {
-      const random = Math.floor(Math.random() * capsArray.length);
-      console.log(random, capsArray[random])
-    }
-
-
-
-
 };
-
 
 
 
@@ -44,7 +59,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); 
+  charLoop();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
