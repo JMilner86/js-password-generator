4e3f5b1edcb50e347f0b90304bdfcec6dcb3da18
Jonah Milner <jonahmilner86@gmail.com>
2022-02-04 13:07:54 -0700

    finished the project, didn't add a lot of commits, but i added a lot of functions, I got it to loop through my if statements, it works...

--- script.js
+++ script.js
@@ -3,15 +3,8 @@ let numArray = ['0','1','2','3','4','5','6','7','8','9'];
 let capsArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 let lowersArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 let specialChars = ['!','@','#','$','%','^','&','*','<','>','?'];
-let emptyArray = [];
 
-const forLoop = (Array) => {
-  for (i = 0; i < Array.length; i++) {
-    console.log(forLoop())
-  }
-  
-}
-forLoop(numArray);
+
 
 const randomItems = (Array) => {
   return Array[Math.floor(Math.random() * Array.length)]
@@ -21,6 +14,7 @@ const randomItems = (Array) => {
 
 
 const generatePassword = () => {
+    let emptyArray = [];
     let passwordLength = window.prompt("What is your desired password length? (8-128 characters). Please choose a number.")
     
      if (isNaN(passwordLength)) {
@@ -35,55 +29,58 @@ const generatePassword = () => {
 
 
 
-
-};
-const charLoop = () => {
     let charChoice = {
       capsLetters: window.confirm("Confirm to add capital letters to your password"),
       lowersLetters: window.confirm("Confirm to add Lower Case letters to your password"),
       includeNumbers: window.confirm("Confirm to add numbers to your password"),
       specials: window.confirm("Confirm to add special characters to your password")
+    };
+    if (!charChoice.capsLetters && !charChoice.lowersLetters && !charChoice.includeNumbers && !charChoice.specials){
+      window.alert("You must choose at least one option, try again.")
+      return generatePassword();
     }
-   
-    if (charChoice.capsLetters) {
+const charLoop = () => {
+    for (i = 0; i < (passwordLength); i++) {
+    if (charChoice.capsLetters && emptyArray.length < passwordLength) {
       let tempVar =  randomItems(capsArray)
-      window.alert("Confirmed capital letters!")
       emptyArray.push(tempVar)
     }
-    if (charChoice.lowersLetters) {
+    if (charChoice.lowersLetters && emptyArray.length < passwordLength) {
       let tempVar = randomItems(lowersArray)
-      window.alert("Confirmed lowercase letters!")
       emptyArray.push(tempVar)
     }
-    if (charChoice.includeNumbers) {
+    if (charChoice.includeNumbers && emptyArray.length < passwordLength) {
       let tempVar = randomItems(numArray) 
-      window.alert("Confirmed numbers!")
       emptyArray.push(tempVar)
     }
-    if(charChoice.specials) {
+    if(charChoice.specials && emptyArray.length < passwordLength) {
       let tempVar = randomItems(specialChars)
-      window.alert("Confirmed special characters!")
       emptyArray.push(tempVar)
-    }
-    else {
-      window.alert("You must choose at least one Character option!")
-      charLoop();
-    }
+    };
+    
+  }
+
 }
 
-console.log(emptyArray)
+charLoop();
+  
 
+  
+    
+ 
+  return emptyArray;
+};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword(); 
-  charLoop();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
+  passwordText.value = password.join('');
+  
 }
 
 // Add event listener to generate button
