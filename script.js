02803d84c3a8e3ccd6e7bad60ff4f1306bbb57bf
Jonah Milner <jonahmilner86@gmail.com>
2022-02-03 21:30:34 -0700

    started writing the code, got the first prompt

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // Assignment code here
+let numArray = ['0','1','2','3','4','5','6','7','8','9'];
+let alphaArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let specialChars = ['!','@','#','$','%','^','&','*','<','>','?'];
+let generatedPassword = [];
+
+const generatePassword = () => {
+    window.prompt("What is your desired password length? (8-128 characters). Please choose a number.")
+};
+
+
+
 
 
 // Get references to the #generate element
